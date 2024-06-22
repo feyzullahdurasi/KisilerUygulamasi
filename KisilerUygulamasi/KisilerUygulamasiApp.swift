e3d5d32d74f3582b5a70a99e5ac1d9d220b2d30c
Feyzullah Durası <durasifeyzullah672@gmail.com>
2024-06-22 08:22:54 +0300

    Initial Commit

---
+++ KisilerUygulamasi/KisilerUygulamasiApp.swift
@@ -0,0 +1,17 @@
+//
+//  KisilerUygulamasiApp.swift
+//  KisilerUygulamasi
+//
+//  Created by Feyzullah Durası on 22.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct KisilerUygulamasiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
